9fe30ab5887bb25ab1b255605ed7bc3dcd27fd35
Vincent Noca <vincent.noca.13@technology-research-services.org>
2017-03-01 22:07:00 -0500

    Modified RegisterActivity, RegisterRequest and Xml

--- app/src/main/java/com/tonikamitv/loginregister/RegisterActivity.java
+++ app/src/main/java/com/tonikamitv/loginregister/RegisterActivity.java
@@ -4,6 +4,7 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,23 +18,27 @@ import org.json.JSONObject;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    private static final String TAG = "RegisterActivity";
+
+    RegisterRequest registerRequest;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        final EditText etAge = (EditText) findViewById(R.id.etAge);
-        final EditText etName = (EditText) findViewById(R.id.etName);
-        final EditText etUsername = (EditText) findViewById(R.id.etUsername);
+        final EditText etFirstName = (EditText) findViewById(R.id.etFirstName);
+        final EditText etLastName = (EditText) findViewById(R.id.etLastName);
+        final EditText etEmail = (EditText) findViewById(R.id.etEmail);
         final EditText etPassword = (EditText) findViewById(R.id.etPassword);
         final Button bRegister = (Button) findViewById(R.id.bRegister);
 
         bRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                final String name = etName.getText().toString();
-                final String username = etUsername.getText().toString();
-                final int age = Integer.parseInt(etAge.getText().toString());
+                final String firstName = etFirstName.getText().toString();
+                final String lastName = etLastName.getText().toString();
+                final String email = etEmail.getText().toString();
                 final String password = etPassword.getText().toString();
 
                 Response.Listener<String> responseListener = new Response.Listener<String>() {
@@ -41,24 +46,34 @@ public class RegisterActivity extends AppCompatActivity {
                     public void onResponse(String response) {
                         try {
                             JSONObject jsonResponse = new JSONObject(response);
-                            boolean success = jsonResponse.getBoolean("success");
-                            if (success) {
-                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                                RegisterActivity.this.startActivity(intent);
-                            } else {
+
+                            //response should hold value returned from php script
+                            // It should have "status" & "message" strings
+                            //Status codes: 400, 200
+                            String status = jsonResponse.getString("status");
+                            String message = jsonResponse.getString("message");
+
+                            if(status.equals("400")){
+                                //It failed
                                 AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                                builder.setMessage("Register Failed")
+                                builder.setTitle("Register Failed")
+                                        .setMessage(message)
                                         .setNegativeButton("Retry", null)
                                         .create()
                                         .show();
+                            }else{
+                                //It succeeded
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                RegisterActivity.this.startActivity(intent);
                             }
                         } catch (JSONException e) {
+                            Log.d(TAG, response);
                             e.printStackTrace();
                         }
                     }
                 };
 
-                RegisterRequest registerRequest = new RegisterRequest(name, username, age, password, responseListener);
+                registerRequest = new RegisterRequest(firstName, lastName, email, password, responseListener);
                 RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
                 queue.add(registerRequest);
             }
--- app/src/main/java/com/tonikamitv/loginregister/RegisterRequest.java
+++ app/src/main/java/com/tonikamitv/loginregister/RegisterRequest.java
@@ -1,22 +1,37 @@
 package com.tonikamitv.loginregister;
 
+import android.util.Log;
+
 import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class RegisterRequest extends StringRequest {
-    private static final String REGISTER_REQUEST_URL = "http://tonikamitv.hostei.com/Register.php";
+
+    private static final String TAG = "RegisterRequest";
+    private static final String REGISTER_REQUEST_URL = "http://10.128.2.19/pb4c/registerUser.php";
+
+
     private Map<String, String> params;
 
-    public RegisterRequest(String name, String username, int age, String password, Response.Listener<String> listener) {
-        super(Method.POST, REGISTER_REQUEST_URL, listener, null);
+    public RegisterRequest(String firstName, String lastName, String email, String password, Response.Listener<String> listener) {
+        //Using Test URL
+        super(Method.POST, REGISTER_REQUEST_URL, listener, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, error.getMessage());
+            }
+        });
         params = new HashMap<>();
-        params.put("name", name);
-        params.put("age", age + "");
-        params.put("username", username);
-        params.put("password", password);
+        params.put("userFirstName", firstName);
+        params.put("userLastName", lastName);
+        params.put("userEmail", email);               //original "name", "age", "username", "password"
+        params.put("userPassword", password);
     }
 
     @Override
